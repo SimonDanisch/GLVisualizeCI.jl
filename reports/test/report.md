d94dc4265ca0225306cbf7b6326d48a261c0836d
SimiDCI <jpanisch@gmail.com>
2017-02-14 10:25:49 -0500

    first test report

---
+++ reports/test/report.md
@@ -0,0 +1,20 @@
+### Test Images:
+<img src="https://github.com/SimiDCI/GLVisualizeCI.jl/blob/master/reports//Users/julia/.julia/v0.5/GLVisualizeCI/reports/test/buttons.jpg?raw=true"
+    alt="buttons.jpg" style="width: 150px;"/>
+
+<img src="https://github.com/SimiDCI/GLVisualizeCI.jl/blob/master/reports//Users/julia/.julia/v0.5/GLVisualizeCI/reports/test/camera.jpg?raw=true"
+    alt="camera.jpg" style="width: 150px;"/>
+
+<img src="https://github.com/SimiDCI/GLVisualizeCI.jl/blob/master/reports//Users/julia/.julia/v0.5/GLVisualizeCI/reports/test/color_chooser.jpg?raw=true"
+    alt="color_chooser.jpg" style="width: 150px;"/>
+
+<img src="https://github.com/SimiDCI/GLVisualizeCI.jl/blob/master/reports//Users/julia/.julia/v0.5/GLVisualizeCI/reports/test/image_processing.jpg?raw=true"
+    alt="image_processing.jpg" style="width: 150px;"/>
+
+<img src="https://github.com/SimiDCI/GLVisualizeCI.jl/blob/master/reports//Users/julia/.julia/v0.5/GLVisualizeCI/reports/test/rotate_robj.jpg?raw=true"
+    alt="rotate_robj.jpg" style="width: 150px;"/>
+
+<img src="https://github.com/SimiDCI/GLVisualizeCI.jl/blob/master/reports//Users/julia/.julia/v0.5/GLVisualizeCI/reports/test/screens.jpg?raw=true"
+    alt="screens.jpg" style="width: 150px;"/>
+
+No failures! :)
--- src/gltest.jl
+++ src/gltest.jl
@@ -2,77 +2,103 @@ using GLVisualize
 include(GLVisualize.dir("examples", "ExampleRunner.jl"))
 using ExampleRunner
 importall ExampleRunner
-
-using GLAbstraction, GLWindow, GLVisualize, Colors
+using GLAbstraction, GLWindow, Colors
 using FileIO, GeometryTypes, Reactive, Images
 
+function image_url(path)
+    "https://github.com/SimiDCI/GLVisualizeCI.jl/blob/master/reports/$(path)?raw=true"
+end
 
-files = [
-    "introduction/rotate_robj.jl",
-    "introduction/screens.jl",
-    "plots/3dplots.jl",
-    "plots/lines_scatter.jl",
-    "plots/hybrid.jl",
-    "camera/camera.jl",
-    "gui/color_chooser.jl",
-    "gui/image_processing.jl",
-    "gui/buttons.jl",
-    "gui/fractal_lines.jl",
-    "gui/mandalas.jl",
-    "plots/drawing.jl",
-    "interactive/graph_editing.jl",
-    "interactive/mario_game.jl",
-    "text/text_particle.jl",
-]
-
-map!(x-> GLVisualize.dir("examples", x), files, files)
-files = union(files, ExampleRunner.flatten_paths(GLVisualize.dir("examples")))
-push!(files, GLVisualize.dir("test", "summary.jl"))
-files = unique(normpath.(files))
-# Create an examplerunner, that displays all examples in the example folder, plus
-# a runtest specific summary.
-ENV["GLVISUALIZE_SCREENCAST_FOLDER"] = joinpath(homedir(), "Desktop", "test")
-config = ExampleRunner.RunnerConfig(
-    record = false,
-    record_image = true,
-    files = files,
-    thumbnail = false,
-    screencast_folder = ENV["GLVISUALIZE_SCREENCAST_FOLDER"],
-    resolution = (200, 200)
-)
-window = config.window
-for path in config.files
-    isopen(config.rootscreen) || break
-    try
-        test_module = ExampleRunner._test_include(path, config)
-        ExampleRunner.display_msg(test_module, config)
-        GLWindow.poll_glfw()
-        GLWindow.poll_reactive()
-        yield()
-        render_frame(config.rootscreen)
-        swapbuffers(config.rootscreen)
-        name = basename(path)[1:end-3]
-        name = joinpath(config.screencast_folder, name * ".png")
-        GLWindow.screenshot(config.rootscreen, path = name)
-    catch e
-        #failed[i] = true
-        bt = catch_backtrace()
-        ex = CapturedException(e, bt)
-        showerror(STDERR, ex)
-        config[:success] = false
-        config[:exception] = ex
-    finally
-        empty!(window)
-        empty!(config.buttons[:timesignal].actions)
-        window.color = RGBA{Float32}(1,1,1,1)
-        window.clear = true
-        GLVisualize.empty_screens!()
-        GLVisualize.add_screen(window) # make window default again!
-        for (k, s) in window.inputs
-            empty!(s.actions)
-        end
-        empty!(window.cameras)
-        gc()
+function create_mosaic(io, folder, width = 150)
+    images = filter(x-> endswith(x, ".jpg"), readdir(folder))
+    n = GLAbstraction.close_to_square(length(images))
+    for im in images
+        println(io, """<img src="$(image_url(joinpath(folder, im)))"
+            alt="$(im)" style="width: $(width)px;"/>
+        """)
     end
-    yield()
+end
+
+function test_and_record(folder)
+    files = [
+        "introduction/rotate_robj.jl",
+        "introduction/screens.jl",
+        "plots/3dplots.jl",
+        "plots/lines_scatter.jl",
+        "plots/hybrid.jl",
+        "camera/camera.jl",
+        "gui/color_chooser.jl",
+        "gui/image_processing.jl",
+        "gui/buttons.jl",
+        "gui/fractal_lines.jl",
+        "gui/mandalas.jl",
+        "plots/drawing.jl",
+        "interactive/graph_editing.jl",
+        "interactive/mario_game.jl",
+        "text/text_particle.jl",
+    ]
+
+    map!(x-> GLVisualize.dir("examples", x), files, files)
+    files = union(files, ExampleRunner.flatten_paths(GLVisualize.dir("examples")))
+    push!(files, GLVisualize.dir("test", "summary.jl"))
+    files = unique(normpath.(files))
+    # Create an examplerunner, that displays all examples in the example folder, plus
+    # a runtest specific summary.
+    config = ExampleRunner.RunnerConfig(
+        record = false,
+        record_image = true,
+        files = files,
+        thumbnail = false,
+        screencast_folder = folder,
+        resolution = (200, 200)
+    )
+    GLWindow.set_visibility!(config.rootscreen, false)
+    window = config.window
+    for path in config.files
+        isopen(config.rootscreen) || break
+        try
+            println(basename(path))
+            test_module = ExampleRunner._test_include(path, config)
+            ExampleRunner.display_msg(test_module, config)
+            GLWindow.poll_glfw()
+            GLWindow.poll_reactive()
+            yield()
+            render_frame(config.rootscreen)
+            swapbuffers(config.rootscreen)
+            name = basename(path)[1:end-3]
+            name = joinpath(config.screencast_folder, name * ".jpg")
+            GLWindow.screenshot(config.rootscreen, path = name)
+        catch e
+            #failed[i] = true
+            bt = catch_backtrace()
+            ex = CapturedException(e, bt)
+            showerror(STDERR, ex)
+            config[:success] = false
+            config[:exception] = ex
+        finally
+            empty!(window)
+            empty!(config.buttons[:timesignal].actions)
+            window.color = RGBA{Float32}(1,1,1,1)
+            window.clear = true
+            GLVisualize.empty_screens!()
+            GLVisualize.add_screen(window) # make window default again!
+            for (k, s) in window.inputs
+                empty!(s.actions)
+            end
+            empty!(window.cameras)
+            gc()
+        end
+        yield()
+    end
+    open(joinpath(screencast_folder, "report.md"), "w") do io
+        failures = filter(config.attributes) do k, dict
+            !dict[:success]
+        end
+        name_ex = map(failures) do k, dict
+            k => dict[:exception]
+        end
+
+
+
+    end
 end
